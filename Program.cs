f122510e50834441f2bae9dd53be742d02a96156
BenGardiner123 <61527372+BenGardiner123@users.noreply.github.com>
2020-07-22 16:13:10 +1000

    working on my random number brute forcer

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace C_Practice
 {
@@ -7,8 +8,8 @@ namespace C_Practice
     {
         static void Main(string[] args)
         {
-            
-            System.Console.WriteLine("Please enter you number");
+            ///prime numbers and factors homework
+           /*  System.Console.WriteLine("Please enter you number");
             string userInput = Console.ReadLine();
             
             if (checkUserInput(userInput))
@@ -17,15 +18,53 @@ namespace C_Practice
                 //factorsOf(newInt);
 
               primeCalc(newInt);  
-            }
+            } */
             
-           
+           ///.!---.!---.!---.!---.!---.!---.!---.!---.!---.!---.!---.!---.!---
+            //SECOND HOMEWORK TASK - Problem 3 - ABCD * E == DCBA
+            /* A, B, C, D, E represent digits.
+            Write a program to find the values for A, B, C, D, E that makes the statement "ABCD * E = DCBA" true.
+            Each digit must be unique. Each digit must be a value from 0 - 9. There is one valid solution.
+            */
+
+           equationcheck();
+
+
+
+
         }
+
+        public static void equationcheck()
+        {
+            var rand = new Random();
+            var guesChecker = new List<string>();
+            var answerChecker = new List<string>();
+            
+            int goOne = rand.Next(1000, 9999);
+            string guess = goOne.ToString();
+            guesChecker.Add(guess);
+            
+            int multipler = rand.Next(1, 9);
+
+            int answer =  goOne * multipler;
+
+            string x = answer.ToString();
+            answerChecker.Add(x);
+            
+            if (x.Length == 4)
+            {
+                string [] reversedAnswer = answerChecker.ToArray();
+                
+            }
+
+
+        }
+
 
         public static void primeCalc(int userInput)
         {
             Console.WriteLine("2");
-            
+
             for (int i = 3; i <= userInput + 1; i++)
             {
                 if(isPrime(i))
